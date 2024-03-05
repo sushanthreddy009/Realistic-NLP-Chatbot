23bb5ed1fcb45a97ce41a3ed7baa7332fea29573
Sushanth Reddy <sushanthreddy717@gmail.com>
2024-03-05 10:43:52 +0530

    Add files via upload

---
+++ nlp/training/estimator.py
@@ -0,0 +1,89 @@
+import tensorflow as tf
+
+from nlp.training.model import Seq2Seq, Seq2SeqAttn
+
+
+class Seq2SeqEstimator(object):
+    def __init__(self, params):
+        self._instantiate_model(params)
+
+    def _instantiate_model(self, params, training=False):
+        if params['model'] == 'seq2seq':
+            self.model = Seq2Seq(params=params, is_training=training)
+        elif params['model'] == 'attn':
+            self.model = Seq2SeqAttn(params=params, is_training=training)
+
+    def _output_network(self, features, params, training=False):
+        self._instantiate_model(params=params, training=training)
+        output = self.model(inputs=features)
+        return output
+
+    @staticmethod
+    def loss_fn(labels, predictions, params):
+        pred = predictions['answer']
+        one_hot_label = labels['one_hot_label']
+
+        loss = None
+        if params['loss'] == 'regular':
+            losses = tf.nn.softmax_cross_entropy_with_logits_v2(labels=one_hot_label, logits=pred)
+            loss = tf.reduce_sum(losses) / params['batch_size']
+        elif params['loss'] == 'masked':
+            label = labels['label']
+            loss_mask = label > 0
+            losses = tf.nn.softmax_cross_entropy_with_logits_v2(labels=one_hot_label, logits=pred)
+            masked_losses = tf.where(loss_mask, losses, tf.zeros_like(losses))
+            loss = tf.reduce_sum(masked_losses) / params['batch_size']
+
+        tf.summary.scalar('loss', tensor=loss)
+        return loss
+
+    @staticmethod
+    def accuracy_fn(labels, predictions, params):
+        pred = predictions['answer']
+        label = labels['label']
+
+        label = tf.expand_dims(label, axis=-1)
+        mask = label > 0
+        pred_val = tf.expand_dims(tf.argmax(tf.nn.softmax(pred), axis=-1), axis=-1)
+
+        masked_pred = tf.squeeze(tf.where(mask, pred_val, tf.zeros_like(pred_val)), axis=-1)
+        total = params['batch_size'] * params['max_len']
+        trues = total - tf.reduce_sum(tf.count_nonzero(tf.subtract(masked_pred, tf.squeeze(label, axis=-1)), axis=-1))
+        accuracy = trues / total
+        return accuracy
+
+    def model_fn(self, features, labels, mode, params):
+        training = (mode == tf.estimator.ModeKeys.TRAIN)
+        preds = self._output_network(features, params, training=training)
+
+        # Training op
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            optimizer = None
+            if params['optimizer'] == 'rmsprop':
+                optimizer = tf.train.RMSPropOptimizer(learning_rate=params['lr'])
+            elif params['optimizer'] == 'adam':
+                optimizer = tf.train.AdamOptimizer(learning_rate=params['lr'])
+
+            predictions = {'answer': preds}
+            loss = self.loss_fn(labels, predictions, params)
+            train_op = tf.contrib.training.create_train_op(loss, optimizer, global_step=tf.train.get_global_step())
+
+            acc = self.accuracy_fn(labels, predictions, params)
+            tf.summary.scalar('acc', tensor=acc, family='accuracy')
+            return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+        # Evaluation op
+        if mode == tf.estimator.ModeKeys.EVAL:
+            predictions = {'answer': preds}
+            loss = self.loss_fn(labels, predictions, params)
+            return tf.estimator.EstimatorSpec(mode=mode, loss=loss)
+
+        # Prediction op
+        if mode == tf.estimator.ModeKeys.PREDICT:
+            predictions = {
+                'input_sentence': features['input_sentence'],
+                'input_target': features['input_target'],
+                'answer': tf.nn.softmax(preds)
+            }
+            export_outputs = {'predictions': tf.estimator.export.PredictOutput(predictions)}
+            return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions, export_outputs=export_outputs)
---
+++ nlp/training/train.py
@@ -0,0 +1,162 @@
+import os
+import glob
+import argparse
+import logging
+import sys
+from datetime import datetime
+
+import numpy as np
+import tensorflow as tf
+import json
+
+from nlp.training.input_pipeline import data_input_fn
+from nlp.training.estimator import Seq2SeqEstimator
+
+
+def main():
+    parser = argparse.ArgumentParser(description='train the NLP model')
+    parser.add_argument('--data-dir', '-d', type=str, default='/home/filippo/datasets/cornell_movie_data/tfrecords/',
+                        help='tf records data directory')
+    parser.add_argument('--model-dir', '-md', type=str, default='', help='pretrained model directory')
+    parser.add_argument('--ckpt', '-c', type=str, default='', help='pretrained checkpoint directory')
+    parser.add_argument('--mode', '-m', type=str, default='train', help='train, eval or predict')
+    parser.add_argument('--model', '-mo', type=str, default='seq2seq', help='model name')
+    parser.add_argument('--batch-size', '-bs', type=int, default=64, help='batch size')
+    parser.add_argument('--epochs', '-e', type=int, default=100, help='train epochs')
+    parser.add_argument('--learning-rate', '-lr', type=float, default=0.0001, help='initial learning rate')
+    parser.add_argument('--max-len', '-ml', type=int, default=10, help='max sentence length')
+    parser.add_argument('--voc-size', '-vs', type=int, default=7826, help='vocabulary size')
+    parser.add_argument('--loss', '-l', type=str, default='regular', help='regular loss or masked loss')
+    parser.add_argument('--optimizer', '-o', type=str, default='rmsprop', help='rmsprop or adam optimizer')
+    parser.add_argument('--fake-input', action='store_true', default=False, help='debug with 1 batch training')
+    args = parser.parse_args()
+
+    assert args.model in ['seq2seq', 'attn'], 'Wrong model name'
+    assert args.loss in ['regular', 'masked'], 'Wrong loss name'
+    assert args.optimizer in ['rmsprop', 'adam'], 'Wrong optimizer name'
+
+    tfrecords_train = glob.glob('{}train/*.tfrecord'.format(args.data_dir))
+    tfrecords_val = glob.glob('{}val/*.tfrecord'.format(args.data_dir))
+    tfrecords_test = glob.glob('{}test/*.tfrecord'.format(args.data_dir))
+
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+    logger = logging.getLogger(__name__)
+    np.random.seed(0)
+    tf.set_random_seed(0)
+    tf.logging.set_verbosity(tf.logging.INFO)
+
+    if not args.model_dir:
+        save_dir = '{}models/{}/{}/'.format(args.data_dir, args.model, datetime.now().isoformat())
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+    else:
+        save_dir = args.model_dir
+
+    params = {
+        'model': args.model,
+        'batch_size': args.batch_size,
+        'epochs': args.epochs,
+        'lr': args.learning_rate,
+        'max_len': args.max_len,
+        'voc_size': args.voc_size,
+        'emb_dim': 256,
+        'enc_dim': 128,
+        'dec_dim': 128,
+        'loss': args.loss,
+        'optimizer': args.optimizer
+    }
+
+    train_config = tf.estimator.RunConfig(save_summary_steps=10,
+                                          save_checkpoints_steps=500,
+                                          keep_checkpoint_max=10,
+                                          log_step_count_steps=10)
+
+    ws = None
+    if args.ckpt:
+        ws = tf.estimator.WarmStartSettings(ckpt_to_initialize_from=args.ckpt, vars_to_warm_start='.*')
+
+    # Create TensorFlow estimator object
+    estimator_obj = Seq2SeqEstimator(params)
+    estimator = tf.estimator.Estimator(model_fn=estimator_obj.model_fn,
+                                       model_dir=save_dir,
+                                       config=train_config,
+                                       params=params,
+                                       warm_start_from=ws)
+
+    mode_keys = {
+        'train': tf.estimator.ModeKeys.TRAIN,
+        'eval': tf.estimator.ModeKeys.EVAL,
+        'predict': tf.estimator.ModeKeys.PREDICT
+    }
+    mode = mode_keys[args.mode]
+
+    # Training & Evaluation on Train / Val set
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        train_input_fn = data_input_fn(tfrecords_train,
+                                       batch_size=params['batch_size'],
+                                       epochs=1,
+                                       max_len=params['max_len'],
+                                       voc_size=params['voc_size'],
+                                       shuffle=True,
+                                       fake_input=args.fake_input)
+        eval_input_fn = data_input_fn(tfrecords_val,
+                                      batch_size=params['batch_size'],
+                                      epochs=1,
+                                      max_len=params['max_len'],
+                                      voc_size=params['voc_size'],
+                                      shuffle=False,
+                                      fake_input=args.fake_input)
+
+        for epoch_num in range(params['epochs']):
+            logger.info("Training for epoch {} ...".format(epoch_num))
+            estimator.train(input_fn=train_input_fn)
+            logger.info("Evaluation for epoch {} ...".format(epoch_num))
+            estimator.evaluate(input_fn=eval_input_fn)
+
+    # Evaluation on Test set
+    elif mode == tf.estimator.ModeKeys.EVAL:
+        test_input_fn = data_input_fn(tfrecords_val,
+                                      batch_size=params['batch_size'],
+                                      epochs=1,
+                                      max_len=params['max_len'],
+                                      voc_size=params['voc_size'],
+                                      shuffle=False,
+                                      fake_input=args.fake_input)
+
+        logger.info("Evaluation of test set ...")
+        estimator.evaluate(input_fn=test_input_fn)
+
+    # Prediction visualization on Test set
+    elif mode == tf.estimator.ModeKeys.PREDICT:
+        test_input_fn = data_input_fn(tfrecords_test,
+                                      batch_size=params['batch_size'],
+                                      epochs=1,
+                                      max_len=params['max_len'],
+                                      voc_size=params['voc_size'],
+                                      shuffle=False,
+                                      fake_input=args.fake_input)
+
+        voc_fp = os.path.join(args.data_dir, 'voc.json')
+        with open(voc_fp, 'r') as f:
+            voc_data = json.load(f)
+
+        predictions = estimator.predict(input_fn=test_input_fn)
+        for n, pred in enumerate(predictions):
+            input_sentence = np.int64(pred['input_sentence'])
+            input_target = np.int64(pred['input_target'])
+            pred = np.int64(np.argmax(pred['answer'], axis=-1))
+
+            input_sentence_words = [voc_data['index2word'][str(x)] for x in input_sentence]
+            input_target_words = [voc_data['index2word'][str(x)] for x in input_target]
+            pred_words = [voc_data['index2word'][str(x)] for x in pred]
+
+            print("\nInputs:")
+            print(input_sentence, input_target)
+            print(input_sentence_words, input_target_words)
+            print("Prediction:")
+            print(pred)
+            print(pred_words)
+
+
+if __name__ == '__main__':
+    main()
